003c754adcf31d0bf80e704cd5590841deb464a2
Charlie Ford <charles.ford1978@gmail.com>
2021-09-28 21:47:16 -0400

    Created Triang class for calculating perimeter and area of a triangle.  Added an instance for Triang in the program.

--- GeometricShapes/Program.cs
+++ GeometricShapes/Program.cs
@@ -13,7 +13,6 @@ namespace GeometricShapes
             Console.WriteLine($"Perimeter is {s1.perimeter()}");
             Console.WriteLine($"Area is {s1.area()}");
 
-
             var r1 = new Rect()
             {
                 side1 = 3, side2 = 5
@@ -21,12 +20,18 @@ namespace GeometricShapes
             Console.WriteLine($"Perimeter is {r1.perimeter()}");
             Console.WriteLine($"Area is {r1.area()}");
 
-
             var q1 = new Quad()
             {
                 side1 = 3, side2 = 4, side3 = 5, side4 = 6
             };
             Console.WriteLine($"Perimeter is {q1.perimeter()}");
+
+            var t1 = new Triang()
+            {
+                Base = 5, side1 = 8, side2 = 9, height = 7
+            };
+            Console.WriteLine($"Perimeter is {t1.perimeter()}");
+            Console.WriteLine($"Area is {t1.area()}");
         }
     }
 }
---
+++ GeometricShapes/Triang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapes
+{
+    class Triang
+    {
+        public int Base { get; set; }
+        public int side1 { get; set; }
+        public int side2 { get; set; }
+        public int height { get; set; }
+
+        public int perimeter()
+        {
+            return Base + side1 + side2;
+        }
+        public int area()
+        {
+            return (Base * height) / 2;
+        }
+    }
+}
